38d63a9216d275722e1004fec4e06571805ef8e8
joycetonzon <Najtonzon06@gmail.com>
2018-05-04 17:29:06 +0800

    ta
    hehe

--- app/modules/secretariat/routes.js
+++ app/modules/secretariat/routes.js
@@ -19,22 +19,22 @@ secretariatRouter.get('/', (req, res)=>{
 //===============================================================================================//
 // A P P O I N T //
 //===============================================================================================//
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/appoint', (req, res)=>{
     res.render('secretariat/views/appoint/coodinator')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/appoint/coordinator', (req, res)=>{
     res.render('secretariat/views/appoint/coordinator')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
-secretariatRouter.get('/appoin/tmember', (req, res)=>{
+
+secretariatRouter.get('/appoint/member', (req, res)=>{
     res.render('secretariat/views/appoint/member')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/appoint/priest', (req, res)=>{
     res.render('secretariat/views/appoint/priest')
 });
@@ -43,7 +43,7 @@ secretariatRouter.get('/appoint/priest', (req, res)=>{
 //===============================================================================================//
 // E V E N T S //
 //===============================================================================================//
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/events', (req, res)=>{
     res.render('secretariat/views/event/sacraments')
 });
@@ -51,7 +51,7 @@ secretariatRouter.get('/events/sacraments', (req, res)=>{
     res.render('secretariat/views/event/sacraments')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/events/seminars', (req, res)=>{
     res.render('secretariat/views/event/seminars')
 });
@@ -60,7 +60,7 @@ secretariatRouter.get('/events/seminars', (req, res)=>{
 //===============================================================================================//
 // R E S E R V A T I O N //
 //===============================================================================================//
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/reservation/', (req, res)=>{
     res.render('secretariat/views/reservation/document')
 });
@@ -68,12 +68,12 @@ secretariatRouter.get('/reservation/document', (req, res)=>{
     res.render('secretariat/views/reservation/document')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/reservation/events', (req, res)=>{
     res.render('secretariat/views/reservation/events')
 });
 
-secretariatRouter.use(authMiddleware.secretariatAuth)
+
 secretariatRouter.get('/reservation/facilities', (req, res)=>{
     res.render('secretariat/views/reservation/facilities')
 });
